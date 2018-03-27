cef72bf5e8d4a02ced14f08cf107401d99bedc08
danialdo95 <danialdo95@gmail.com>
2018-03-27 13:44:53 +0800

    Update walk animation and added run and jump animation

--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -9,11 +9,13 @@ public class player : MonoBehaviour {
 
     private float inputH;
     private float inputV;
-
+    private bool run;
 	// Use this for initialization
 	void Start () {
+
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
+        run = false;
 	}
 	
 	// Update is called once per frame
@@ -49,16 +51,48 @@ public class player : MonoBehaviour {
             
         }
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+
+
         inputH = Input.GetAxis("Horizontal");
         inputV = Input.GetAxis("Vertical");
 
         anim.SetFloat("inputH", inputH);
         anim.SetFloat("inputV", inputV);
+        anim.SetBool("run", run);
 
         float moveX = inputH*20f*Time.deltaTime;
         float moveZ = inputV * 50f * Time.deltaTime;
 
+        if(moveZ <= 0f)
+        {
+            moveX = 0f;
+        }
+        else if(run)
+        {
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
+
         rbody.velocity = new Vector3(moveX, 0f,moveZ);
+
+        
 
     }
 }
